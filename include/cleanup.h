55649344d4e8433763da2015045596741bdea04b
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-09-25 17:22:59 +0530

    refactor: add remaining header files to cleanup.h

--- include/cleanup.h
+++ include/cleanup.h
@@ -1,6 +1,17 @@
 #ifndef CLEANUP_H
 #define CLEANUP_H
 
-#include "../include/utils.h"
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "utils.h"
 
 #endif
