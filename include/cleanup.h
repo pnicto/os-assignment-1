721827a51f1cf23a4caecbe60631ad5cffd99e25
Anurav-Garg <anurav.grg@gmail.com>
2023-09-26 21:22:04 +0530

    refactor: remove unnecessary includes in cleanup.h

--- include/cleanup.h
+++ include/cleanup.h
@@ -1,16 +1,11 @@
 #ifndef CLEANUP_H
 #define CLEANUP_H
 
-#include <errno.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 
 #include "utils.h"
 
